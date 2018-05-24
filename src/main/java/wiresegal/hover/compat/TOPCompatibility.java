2be5b9d9f23b6af060dd57e997ac450072ada1db
Andrew W <mallrat208@users.noreply.github.com>
2018-05-24 09:10:07 -0400

    Add The One Probe Support (#1)
    * Adde TheOneProbe Support
    
    Added en_us localization for the hoverboard entity
    Added The One Probe support for the Hoverboard Entity, looking at the Hoverboard will display it's current Energy amount
    
    * Update TOP Support
    
    Update TOP to indicate if a hoverboard is Creative and therefore not show Energy information

---
+++ src/main/java/wiresegal/hover/compat/TOPCompatibility.java
@@ -0,0 +1,64 @@
+package wiresegal.hover.compat;
+
+import com.google.common.base.Function;
+import mcjty.theoneprobe.api.IProbeHitEntityData;
+import mcjty.theoneprobe.api.IProbeInfo;
+import mcjty.theoneprobe.api.IProbeInfoEntityProvider;
+import mcjty.theoneprobe.api.IProgressStyle;
+import mcjty.theoneprobe.api.ITheOneProbe;
+import mcjty.theoneprobe.api.ProbeMode;
+import mcjty.theoneprobe.apiimpl.TheOneProbeImp;
+import mcjty.theoneprobe.apiimpl.elements.ElementProgress;
+import mcjty.theoneprobe.config.Config;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.energy.IEnergyStorage;
+import wiresegal.hover.Hoverboard;
+import wiresegal.hover.entity.EntityHoverboard;
+import wiresegal.hover.item.ItemHoverboard;
+
+import javax.annotation.Nullable;
+
+
+public class TOPCompatibility implements Function<ITheOneProbe, Void> {
+
+		@Nullable
+		@Override
+		public Void apply(ITheOneProbe theOneProbe) {
+			theOneProbe.registerEntityProvider(new IProbeInfoEntityProvider() {
+				@Override
+				public String getID() {
+					return Hoverboard.MOD_ID+":default";
+				}
+				
+				@Override
+				public void addProbeEntityInfo(ProbeMode probeMode, IProbeInfo iProbeInfo, EntityPlayer entityPlayer, World world, Entity entity, IProbeHitEntityData iProbeHitEntityData) {
+					if(entity instanceof EntityHoverboard) {
+						int currentPower = ((EntityHoverboard)entity).getPower();
+						int maxPower = ((EntityHoverboard)entity).getMaxFuel();
+						boolean creative = ((EntityHoverboard)entity).isCreative();
+						
+						if(!creative) {
+							iProbeInfo.progress(currentPower, maxPower, iProbeInfo
+									.defaultProgressStyle()
+									.suffix("FE")
+									.filledColor(Config.rfbarFilledColor)
+									.alternateFilledColor(Config.rfbarAlternateFilledColor)
+									.borderColor(Config.rfbarBorderColor)
+									.numberFormat(Config.rfFormat));
+						} else {
+							iProbeInfo.text(TextFormatting.LIGHT_PURPLE + "Creative");
+						}
+					}
+				}
+				
+				
+			});
+			
+			return null;
+	}
+}
